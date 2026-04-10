8cf76141da0e591dd030606268e5989558b70a30
Frehiwot Asfaw <frehiwotasfaw@Frehiwots-MacBook-Pro.local>
2026-04-09 22:07:26 -0400

    Revert "app"
    This reverts commit ed7a83c4c857826d958cbd6aacc50e75bef79d7b.

--- react-redux-saga-crud-app/src/App.js
+++ react-redux-saga-crud-app/src/App.js
@@ -3,20 +3,21 @@ import { Provider } from "react-redux"
 import MyForm from "./components/MyForm"
 import MyTable from "./components/MyTable"
 import store from './store'
-const Apps = () => {
-    return <>
-        <Provider store={store}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={4} lg={6}>
-                    <MyForm />
-                </Grid>
-                <Grid item xs={12} md={8} lg={6}>
-                    <MyTable />
-                </Grid>
-            </Grid>
-        </Provider>
-    </>
-}
+// const App = () => {
+//     return <>
+//         <Provider store={store}>
+//             <Grid container spacing={2}>
+//                 <Grid item xs={12} md={4} lg={6}>
+//                     <MyForm />
+//                 </Grid>
+//                 <Grid item xs={12} md={8} lg={6}>
+//                     <MyTable />
+//                 </Grid>
+//             </Grid>
+//         </Provider>
+//     </>
+// }
+// export default App
 
 
 import * as React from 'react';
