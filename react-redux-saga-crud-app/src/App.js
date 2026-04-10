ed7a83c4c857826d958cbd6aacc50e75bef79d7b
Frehiwot Asfaw <frehiwotasfaw@Frehiwots-MacBook-Pro.local>
2026-04-09 22:06:45 -0400

    app

--- react-redux-saga-crud-app/src/App.js
+++ react-redux-saga-crud-app/src/App.js
@@ -3,21 +3,20 @@ import { Provider } from "react-redux"
 import MyForm from "./components/MyForm"
 import MyTable from "./components/MyTable"
 import store from './store'
-// const App = () => {
-//     return <>
-//         <Provider store={store}>
-//             <Grid container spacing={2}>
-//                 <Grid item xs={12} md={4} lg={6}>
-//                     <MyForm />
-//                 </Grid>
-//                 <Grid item xs={12} md={8} lg={6}>
-//                     <MyTable />
-//                 </Grid>
-//             </Grid>
-//         </Provider>
-//     </>
-// }
-// export default App
+const Apps = () => {
+    return <>
+        <Provider store={store}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={4} lg={6}>
+                    <MyForm />
+                </Grid>
+                <Grid item xs={12} md={8} lg={6}>
+                    <MyTable />
+                </Grid>
+            </Grid>
+        </Provider>
+    </>
+}
 
 
 import * as React from 'react';
