5ed556423b5eb704700d6838991d53bef9adbe78
Frehiwot Asfaw <frehiwotasfaw@mac.mynetworksettings.com>
2026-04-24 21:19:16 -0400

    last commit

---
+++ I94Form/traveler-backend/src/test/java/com/fre/traveler_backend/TravelerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fre.traveler_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
